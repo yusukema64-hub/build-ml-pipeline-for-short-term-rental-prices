216fa8add618d19b9444242f5eb2b2ff96e6e08b
yusukema64-hub <yusuke.ma64@gmail.com>
2026-06-30 07:45:47 +0900

    Update test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -4,7 +4,6 @@ import scipy.stats
 
 
 def test_column_names(data):
-
     expected_colums = [
         "id",
         "name",
@@ -23,43 +22,30 @@ def test_column_names(data):
         "calculated_host_listings_count",
         "availability_365",
     ]
-
-    these_columns = data.columns.to_numpy()  # Using to_numpy() for better performance
-
-    # This also enforces the same order
-    assert np.array_equal(expected_colums, these_columns)  # Using numpy comparison for better performance
+    these_columns = data.columns.to_numpy()
+    assert np.array_equal(expected_colums, these_columns)
 
 
 def test_neighborhood_names(data):
-
     known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
-
     neigh = set(data['neighbourhood_group'].unique())
-
-    # Unordered check
     assert set(known_names) == set(neigh)
 
 
 def test_proper_boundaries(data: pd.DataFrame):
-    """
-    Test proper longitude and latitude boundaries for properties in and around NYC
-    """
     idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
-
     assert np.sum(~idx) == 0
 
 
 def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_threshold: float):
-    """
-    Apply a threshold on the KL divergence to detect if the distribution of the new data is
-    significantly different than that of the reference dataset
-    """
     dist1 = data['neighbourhood_group'].value_counts().sort_index()
     dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
-
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all()
